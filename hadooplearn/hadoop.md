633cc2791fbaa09e29d6e87e7c02f24f7241bcf7
kongming <kongming@jd.com>
2016-10-23 22:16:22 +0800

    hadoop Signed-off-by: kongming <kongming@jd.com>

---
+++ hadooplearn/hadoop.md
@@ -0,0 +1,4 @@
+/export/servers/hadoop-2.7.2/sbin/hadoop-daemon.sh start namenode
+/export/servers/hadoop-2.7.2/sbin/hadoop-daemon.sh start datanode
+/export/servers/hadoop-2.7.2/sbin/yarn-daemon.sh start resourcemanager
+/export/servers/hadoop-2.7.2/sbin/yarn-daemon.sh start nodemanager
---
+++ hadooplearn/src/main/java/com/github/thushear/bigdata/HadooplearnApplication.java
@@ -0,0 +1,12 @@
+package com.github.thushear.bigdata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HadooplearnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HadooplearnApplication.class, args);
+	}
+}
