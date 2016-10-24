0d58df797179217afcd4e2cdad7bc78efbb5da82
kongming <kongming@jd.com>
2016-10-25 06:59:50 +0800

    HdfsApiMan Signed-off-by: kongming <kongming@jd.com>

--- hadooplearn/src/main/java/com/github/thushear/bigdata/hdfs/HdfsApiMan.java
+++ hadooplearn/src/main/java/com/github/thushear/bigdata/hdfs/HdfsApiMan.java
@@ -1,7 +1,10 @@
 package com.github.thushear.bigdata.hdfs;
 
 import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FSDataInputStream;
 import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IOUtils;
 
 import java.io.IOException;
 
@@ -12,13 +15,31 @@ public class HdfsApiMan {
 
 
   public static void main(String[] args) throws IOException {
-
+    String fileName = "/user/testfile.txt";
     FileSystem fileSystem = getDFSFileSystem();
-
     System.out.println("fileSystem = " + fileSystem);
+    Path path = new Path(fileName);
+    FSDataInputStream fsDataInputStream = fileSystem.open(path);
+
+    try {
+      IOUtils.copyBytes(fsDataInputStream, System.out, 4096, false);
+    } catch (IOException e) {
+      e.printStackTrace();
+    } finally {
+      IOUtils.closeStream(fsDataInputStream);
+    }
+
+//    System.out.println("fileSystem = " + fileSystem);
 
   }
 
+
+  /**
+   * 获取HDFS文件系统
+   *
+   * @return
+   * @throws IOException
+   */
   private static FileSystem getDFSFileSystem() throws IOException {
 
     Configuration configuration = new Configuration();
