643ed743c6d959d173a5e53ea795796a9beb2544
kongming <kongming@jd.com>
2016-12-13 22:20:28 +0800

    scala

---
+++ scalaman/src/com/github/thushear/man/HelloWorld.scala
@@ -0,0 +1,12 @@
+package com.github.thushear.man
+
+/**
+  * Created by kongming on 2016/12/13.
+  */
+object HelloWorld {
+
+  def main(args: Array[String]): Unit = {
+     println("hello world!")
+  }
+
+}
